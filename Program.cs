88a4f5caa4795f2596e8944d9fe5eafa6ab6c8c2
Brandon <brandon139@protonmail.com>
2024-02-12 21:48:56 +1100

    all page

--- Program.cs
+++ Program.cs
@@ -3,10 +3,22 @@ using System.Text.Json;
 
 HttpClient _httpClient = new HttpClient();
 
-var htmlString = await Scrape("https://www.aliexpress.com/item/1005005669534694.html");
-AliexpressItem item = JsonSerializer.Deserialize<AliexpressItem>(htmlString);
 
-Console.WriteLine(item.PriceComponent.OrigPrice.MaxAmount.Value);
+//?spm=a2g0o.detail.1000061.2.f17cDS29DS29OJ&shop_sortType=bestmatch_sort
+
+var htmlString = await Scrape("https://www.aliexpress.com/item/1005006467826169.html");
+AliexpressItem item = JsonSerializer.Deserialize<AliexpressItem>(htmlString);
+Console.WriteLine(htmlStrinsg);
+File.WriteAllText("../../../X.html", htmlStrinsg);
+//Console.WriteLine(item.PriceComponent.OrigPrice.MaxAmount.Value);
+//Console.WriteLine(item.PriceComponent.OrigPrice.MinAmount.Value);
+//Console.WriteLine(item.PriceComponent.OrigPrice.MinPrice.Value);
+//Console.WriteLine(item.PriceComponent.OrigPrice.MaxPrice.Value);
+
+//Console.WriteLine(item.PriceComponent.DiscountPrice.MinActivityAmount.Value);
+//Console.WriteLine(item.PriceComponent.DiscountPrice.MaxActivityAmount.Value);
+
+//Console.WriteLine(JsonSerializer.Serialize(item.PriceComponent, new JsonSerializerOptions() { WriteIndented=true }));
 
 async Task<string> Scrape(string product)
 {
@@ -19,5 +31,11 @@ async Task<string> Scrape(string product)
     content = content[..(end + 1)];
     content = content.Trim();
     content = content[..^1];
+    return content;
+}
+
+async Task<string> ScrapeAll(string products)
+{
+    string content = await _httpClient.GetStringAsync(products);
     return content;
 }
